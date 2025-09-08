cbae4eaadeac6d38efb87ce81356bed0b89b91df
ömer hilmi hamamcıoğlu <ohhamamcioglu@gmail.com>
2025-09-08 13:06:50 +0300

    feat: add routing and Success page scaffold

---
+++ src/components/Success.jsx
@@ -0,0 +1,9 @@
+export default function Success() {
+  return (
+    <div>
+      <h2 data-cy="success-title">Success</h2>
+      <p data-cy="success-message">Form başarıyla gönderildi</p>
+    </div>
+  )
+}
+
